5d1a645da62a46b80cd21ba6e22e0720b0bd9e0d
baptiste <baptiste.urbain@gmail.com>
2017-05-18 11:17:02 +0200

    Add TD 2017 solution

---
+++ MasterDetail2017/MasterDetail2017/Model/PersonneModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterDetail2017.Model
+{
+    public class PersonneModel
+    {
+        private string _nom;
+        private string _prenom;
+        public string Nom
+        {
+            get
+            {
+                return _nom;
+            }
+
+            set
+            {
+                _nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return _prenom;
+            }
+
+            set
+            {
+                _prenom = value;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Nom} - {Prenom}";
+        }
+    }
+}
---
+++ MasterDetail2017/MasterDetail2017/ViewModels/MasterDetailViewModel.cs
@@ -0,0 +1,123 @@
+﻿using MasterDetail2017.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterDetail2017.ViewModels
+{
+    /// <summary>
+    /// View model
+    /// </summary>
+    public class MasterDetailViewModel : INotifyPropertyChanged
+    {
+        #region private
+        private string _masterDetailLabel;
+        private List<PersonneModel> _listPersonne;
+        private string _nom;
+        private string _nom1;
+        private PersonneModel _selectedPersonne;
+
+        #endregion
+
+        #region Public
+        public string MasterDetailLabel
+        {
+            get
+            {
+                return _masterDetailLabel;
+            }
+
+            set
+            {
+                _masterDetailLabel = value;
+            }
+        }
+
+        public List<PersonneModel> ListPersonne
+        {
+            get
+            {
+                return _listPersonne;
+            }
+
+            set
+            {
+                _listPersonne = value;
+            }
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return _nom;
+            }
+
+            set
+            {
+                _nom = value;
+                Nom1 = value;
+            }
+        }
+
+        public PersonneModel SelectedPersonne
+        {
+            get
+            {
+                return _selectedPersonne;
+            }
+
+            set
+            {
+                _selectedPersonne = value;
+            }
+        }
+
+        public string Nom1
+        {
+            get
+            {
+                return _nom1;
+            }
+
+            set
+            {
+                _nom1 = value;
+                NotifyPropertyChanged("Nom1");
+            }
+        }
+
+        #endregion
+
+        public MasterDetailViewModel()
+        {
+            MasterDetailLabel = "MasterDetail2017";
+            ListPersonne = new List<PersonneModel>()
+            {
+                new PersonneModel()
+                {
+                    Nom = "URBAIN",
+                    Prenom ="BAPTISTE"
+                },
+                new PersonneModel()
+                {
+                    Nom = "BAILLY",
+                    Prenom ="THIBAUT"
+                }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(String info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
